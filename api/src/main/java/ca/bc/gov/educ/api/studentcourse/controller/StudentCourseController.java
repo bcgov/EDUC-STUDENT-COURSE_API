e17222ffbd937ff85187a5130428ac6b84882b90
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-03-03 12:16:06 -0800

    Bug fix changes

--- api/src/main/java/ca/bc/gov/educ/api/studentcourse/controller/StudentCourseController.java
+++ api/src/main/java/ca/bc/gov/educ/api/studentcourse/controller/StudentCourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ca.bc.gov.educ.api.studentcourse.model.dto.StudentCourse;
 import ca.bc.gov.educ.api.studentcourse.service.StudentCourseService;
+import ca.bc.gov.educ.api.studentcourse.util.GradValidation;
+import ca.bc.gov.educ.api.studentcourse.util.ResponseHelper;
 import ca.bc.gov.educ.api.studentcourse.util.StudentCourseApiConstants;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -33,13 +36,23 @@ public class StudentCourseController {
 
     @Autowired
     StudentCourseService studentCourseService;
+    
+    @Autowired
+	GradValidation validation;
+    
+    @Autowired
+	ResponseHelper response;
 
     @GetMapping(StudentCourseApiConstants.GET_STUDENT_COURSE_BY_PEN_MAPPING)
     @PreAuthorize("#oauth2.hasScope('READ_GRAD_STUDENT_COURSE_DATA')")
-    public List<StudentCourse> getStudentCourseByPEN(@PathVariable String pen) {
-        logger.debug("#Get All Course Achievements by PEN: " + pen);
+    public ResponseEntity<List<StudentCourse>> getStudentCourseByPEN(@PathVariable String pen) {
+        logger.debug("#Get All Student Course by PEN: " + pen);
         OAuth2AuthenticationDetails auth = (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails(); 
     	String accessToken = auth.getTokenValue();
-        return studentCourseService.getStudentCourseList(pen,accessToken);
+    	List<StudentCourse> studentCourseList = studentCourseService.getStudentCourseList(pen,accessToken);
+        if(studentCourseList.isEmpty()) {
+        	return response.NOT_FOUND();
+        }
+    	return response.GET(studentCourseList);
     }
 }
